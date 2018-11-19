86947f5408263d1b2e7a7a1c9f8877685b522dcf
boris_zapekanus <aizikovich4@gmail.com>
2018-11-19 19:06:34 +0300

    add function find 'include' string

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,6 +8,7 @@
 
 using namespace std;
 using namespace boost::program_options;
+using namespace boost::filesystem;
 
 int check_exist_dirs(vector<std::string> &sources)
 {
@@ -20,58 +22,106 @@ int check_exist_dirs(vector<std::string> &sources)
         }
     }
     if (!sources.size())
-        return 1;
+        return -1;
     return 0;
 }
 
 void recursive_add_dirs(vector<std::string> &sources)
 {
-    using namespace boost::filesystem;
+
     for(auto &it: sources){
         recursive_directory_iterator dir( it), end;
         while (dir != end)
         {
-            dir->path().exte
+            //dir->path().exte
 
-            //++dir;
+            ++dir;
         }
     }
 
 }
 
+vector<path> collect_source(string &path_to_source)
+{
+    vector<path> sources_cpp;
+        recursive_directory_iterator dir( path_to_source), end;
+        while (dir != end)
+        {
+            if (dir->path().extension() == ".cpp")
+            {
+                sources_cpp.push_back(dir->path());
+            }
+            ++dir;
+        }
+
+        for(auto &dir: sources_cpp)
+        {
+                //cout<<dir<<endl;
+            // Open the File
+            ifstream in(dir.string());
+            // Check if object is valid
+            if(!in)
+            {
+                cout << "Cannot open the File : "<<dir<<endl;
+                continue;
+            }
+            string str;
+             // Read the next line from File untill it reaches the end.
+             while (getline(in, str))
+             {
+
+                   size_t pos = str.find("#include");
+                   if (pos != std::string::npos)
+                       cout << str << endl;
+             }
+             //Close The File
+             in.close();
+
+        }
+
+        return sources_cpp;
+}
+
+
 int main(int argc, char* argv[])
 {
-    vector<std::string> source_dir;
+    vector<std::string> dirs_headers{};
+    string source_dir = "";
     try {
         if(argc < 2)
         {
             cout<< "need more params"<<endl;
-            return 1;
+            return -1;
         }
         options_description desc("Allowed options");
         desc.add_options()
         ("help,h", "print usage message")
-        ("input,I",  value(&source_dir), "Input directory for headers file")
+        ("input,I",  value(&dirs_headers), "Input directory for headers file")
         ;
         variables_map vm;
         store(parse_command_line(argc, argv, desc), vm);
         if (vm.count("input"))
         {
-            source_dir = vm["input"].as< vector<string> >();
+            dirs_headers = vm["input"].as< vector<string> >();
         }
-        source_dir.push_back(argv[1]);
 
-        if(check_exist_dirs(source_dir))
-            return 1;
+        //check existing source directory (it's second parameters in argv). If it does't exist - next actions is mindless.
+        source_dir = argv[1];
+        if ( !boost::filesystem::exists( source_dir) )
+        {
+            return -1;
+        }
+        //check existing headers directories - if they doesn't exist - there is no place for search out headers.
+        if(check_exist_dirs(dirs_headers))
+            return -1;
     }
     catch(exception& e) {
         cerr << e.what() << "\n";
     }
 
-    recursive_add_dirs(source_dir);
 
-    for(auto &t: source_dir)
-        cout<<t<< endl;
+    recursive_add_dirs(dirs_headers);
+    collect_source(source_dir);
 
     return 0;
 
